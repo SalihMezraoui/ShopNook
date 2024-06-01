94446cf8a434d90d97babaf988fd93c0fa441057
salahmms <salih.mezraoui@gmail.com>
2024-06-01 16:01:34 +0200

    Implementing: loading images dynamically from Database, search for products by category

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -3,25 +3,45 @@ import { Injectable } from '@angular/core';
 import { Product } from '../utilities/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../utilities/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService 
 {
+  
   private apiUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.apiUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> 
+  {
+    // building the url based on the category id
+    const searchUrl = `${this.apiUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetProductsResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetProductCategoryResponse>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetProductsResponse {
   _embedded: {
     products: Product[];
   }
+}
+
+  interface GetProductCategoryResponse {
+    _embedded: {
+      productCategory: ProductCategory[];
+    }
 }
