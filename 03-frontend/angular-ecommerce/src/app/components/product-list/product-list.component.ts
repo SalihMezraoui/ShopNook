4d640715925919180f1de74eaff5d07ea54eb557
salahmms <salih.mezraoui@gmail.com>
2024-06-12 03:30:46 +0200

    Search by name implemented

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,28 +12,53 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMethod: boolean = false;
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(() => {this.listProducts();
-    })  
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
+
+    this.searchMethod = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMethod){
+      this.manageSearchProducts();
+    }
+    else{
+      this.manageProductList();
+    }
+  }
+
+  manageSearchProducts(){
+    const myKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+
+    // searching for item based on the given keyword
+    this.productService.searchProducts(myKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+
+    );
+  }
+  manageProductList() {
     // check if id is avialable
     const hasCategoryId: Boolean = this.route.snapshot.paramMap.has('id')
 
-    if(hasCategoryId)
-    {
+    if (hasCategoryId) {
       // get the "id" param string. convert string to  number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     }
-    else{
+    else {
       // no category id available => default to tcategory 1
-      this.currentCategoryId=1;
+      this.currentCategoryId = 1;
     }
 
     // now get the products for the given category
