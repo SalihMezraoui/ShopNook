dd945caacee42aad2fd110ccc61ed286381a81db
Imad-Eddine Abdessami <mdbd1228@hochschule-trier.de>
2024-08-28 19:51:21 +0200

    cleaning

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ShopNook/ecommerce/service/CheckoutServiceImplementation.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ShopNook/ecommerce/service/CheckoutServiceImplementation.java
@@ -32,60 +32,60 @@ public class CheckoutServiceImplementation implements  CheckoutService
 
     @Override
     @Transactional
-public PurchaseResult submitOrder(Purchase purchase)
-{
-    // get the order info from the dto
-    Order order = purchase.getOrder();
-
-    // generate tracking reference
-    String orderTrackingReference = generateOrderTrackingReference();
-    order.setOrderTrackingReference(orderTrackingReference);
-
-    // populating the order with orderItems
-    Set<OrderItem> orderItemSet = purchase.getOrderItemSet();
-    if (orderItemSet != null) {
-        orderItemSet.forEach(order::add);
-    }
-    // populating order with the Address of delivery and payment
-    order.setPaymentAddress(purchase.getPaymentAddress());
-    order.setDeliveryAddress(purchase.getDeliveryAddress());
-
-    // populating the customer with the order
-    Customer customer = purchase.getCustomer();
-
-    // check if this customer already made a purchase
-    String c_email = customer.getEmail();
-
-    Customer customerAlreadyInTheDb = customerRepository.findByEmail(c_email);
-
-    if(customerAlreadyInTheDb != null)
+    public PurchaseResult submitOrder(Purchase purchase)
     {
-        // we found our customer
-        customer = customerAlreadyInTheDb;
+        // get the order info from the dto
+        Order order = purchase.getOrder();
+
+        // generate tracking reference
+        String orderTrackingReference = generateOrderTrackingReference();
+        order.setOrderTrackingReference(orderTrackingReference);
+
+        // populating the order with orderItems
+        Set<OrderItem> orderItemSet = purchase.getOrderItemSet();
+        if (orderItemSet != null) {
+            orderItemSet.forEach(order::add);
+        }
+        // populating order with the Address of delivery and payment
+        order.setPaymentAddress(purchase.getPaymentAddress());
+        order.setDeliveryAddress(purchase.getDeliveryAddress());
+
+        // populating the customer with the order
+        Customer customer = purchase.getCustomer();
+
+        // check if this customer already made a purchase
+        String c_email = customer.getEmail();
+
+        Customer customerAlreadyInTheDb = customerRepository.findByEmail(c_email);
+
+        if(customerAlreadyInTheDb != null)
+        {
+            // we found our customer
+            customer = customerAlreadyInTheDb;
+        }
+        customer.add(order);
+
+        // saving into the DB
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResult(orderTrackingReference);
     }
-    customer.add(order);
-
-    // saving into the DB
-    customerRepository.save(customer);
-
-    // return a response
-    return new PurchaseResult(orderTrackingReference);
-}
 
     @Override
-public PaymentIntent generatePaymentIntent(PaymentInformation paymentInformation) throws StripeException {
-    List<String> supportedPaymentMethods = new ArrayList<>();
-    supportedPaymentMethods.add("card");
+    public PaymentIntent generatePaymentIntent(PaymentInformation paymentInformation) throws StripeException {
+        List<String> supportedPaymentMethods = new ArrayList<>();
+        supportedPaymentMethods.add("card");
 
-    Map<String, Object> params = new HashMap<>();
-    params.put("amount", paymentInformation.getAmount());
-    params.put("currency", paymentInformation.getCurrency());
-    params.put("payment_method_types", supportedPaymentMethods);
-    params.put("description", "Shopnook purchase");
-    params.put("receipt_email", paymentInformation.getReceiptEmail());
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInformation.getAmount());
+        params.put("currency", paymentInformation.getCurrency());
+        params.put("payment_method_types", supportedPaymentMethods);
+        params.put("description", "Shopnook purchase");
+        params.put("receipt_email", paymentInformation.getReceiptEmail());
 
-    return PaymentIntent.create(params);
-}
+        return PaymentIntent.create(params);
+    }
 
     private String generateOrderTrackingReference()
     {
